e7117b9019a1734a8f4369ed2466a71a7b858e27
yaseen-8547 <ycs613678@gmail.com>
2026-04-03 22:03:50 +0530

    sign in page completed ,google auth pending

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,6 +1,47 @@
 
 import google from "../assets/Google-logo.png"
+import { useState } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 export default function Signin (){
+    const navigate=useNavigate()
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const [emailErr,setEmailErr]=useState("")
+    const [passErr,setPassErr]=useState("")
+    const [signinStatus,setsigninStatus]=useState("")
+    const [isSuccess,setIsSuccess]=useState(false)
+    const handleSignin=()=>{
+       if(!email.endsWith("@gmail.com")||email.length<"@gmail.com".length){
+        setEmailErr("provide Valid email")
+       }
+       else if(password.length===0){
+        setPassErr("provide password")
+       }
+       else{
+        axios.post("http://localhost:5000/usersignin",{email:email,password: password})
+        .then((response)=>{
+            const token =response.data.token
+            localStorage.setItem("token",token)
+        
+            setTimeout(() => {
+                navigate("/home")
+            },2000);
+            setsigninStatus("sign in successfull")
+            setIsSuccess(true)
+        })
+        .catch((err)=>{
+            {
+                console.log(err)
+           setsigninStatus("sign in failed")
+           setIsSuccess(false)
+        }
+        })
+       }
+
+    }
+    
+    
     return (
         <>
          <>
@@ -10,16 +51,19 @@ export default function Signin (){
                             <img src={google} alt="" className="object-contain cursor-pointer ml-3 hover:translate-y-2 hover:skew-1 " />
                             <p className=" mr-1 text-white">or</p>
                             <div className=" md:w-lg">
-                                <div className="h-20  w-full max-w-lg   flex flex-col justify-start gap-2 px-4 ">
+                                <div className="h-27  w-full max-w-lg   flex flex-col justify-start gap-2 px-4 ">
                                     <label className="text-white text-left ">Email</label>
-                                    <input type="text" className="w-full max-w-lg  pl-3 h-10 bg-black  text-white border border-white border-solid" placeholder="xyz@gmail.com" />
+                                    <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}}     className="w-full max-w-lg  pl-3 h-10 bg-black  text-white border border-white border-solid" placeholder="xyz@gmail.com" />
+                                    {emailErr&&<p className="text-red-500 text-sm ">{emailErr}</p>}
                                 </div>
-                                <div className="h-20 w-full max-w-lg flex flex-col justify-start gap-2 px-4">
+                                <div className="h-25 w-full max-w-lg flex flex-col justify-start gap-2 px-4">
                                     <label className="text-white text-left ">Password</label>
-                                    <input type="password" className=" pl-3 w-full max-w-lg  h-10 bg-black  text-white border border-white border-solid" placeholder="(eg:world is not enough) "/>
+                                    <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}className=" pl-3 w-full max-w-lg  h-10 bg-black  text-white border border-white border-solid" placeholder="(eg:world is not enough) "/>
+                                    {passErr&&<p className="text-red-500 text-sm ">{passErr}</p>}
                                 </div>
                                 
-                                <button className="w-36  text-lg mt-5 h-12 bg-white  font-thin hover:bg-black hover:text-white hover:translate-0.5 ml-4">Sign in</button>
+                                <button className="w-36  text-lg mt-5 h-12 bg-white  font-thin hover:bg-black hover:text-white hover:translate-0.5 ml-4" onClick={handleSignin}>Sign in</button>
+                                {signinStatus&&isSuccess!==null&&<p className={`${isSuccess?"text-green-500":"text-red-500"} ml-4 text-sm`}>{signinStatus}</p>}
                                 <p className="text-left ml-4 text-white mt-3">need an account  <a  href="/signup" className="text-blue-900 hover:text-blue-400">Sign up</a></p>
                                 <p className="text-left ml-4 text-white mt-3">forget password  <a className="text-blue-900 hover:text-blue-400">reset it</a></p>
                                 
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -3,44 +3,51 @@ import { useState } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 export default function Signup() {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     const [mail, setMail] = useState("")
     const [pass, setPass] = useState("")
     const [err, setErr] = useState("")
     const [passErr, setPassErr] = useState("")
-    const [confPass,setConfPass]=useState("")
-    const [passMissMatch,setPassMissMath]=useState("")
-    const [signupStatus,setSignupStatus]=useState("")
+    const [confPass, setConfPass] = useState("")
+    const [passMissMatch, setPassMissMath] = useState("")
+    const [signupStatus, setSignupStatus] = useState("")
+    const [isSignup,setIsSignup]=useState(null)
     const handleCreateAccount = () => {
-        if (mail.includes("gmail.com")) {
-            setErr("")
-        }
-        else {
-            setErr("provide a valid Email")
-        }
-        if (!pass) {
-            setPassErr("provide password")
-
-        }
-        else {
-            setPassErr("")
-        }
-        if(pass===confPass){
-          setPassMissMath("")
-        }
-        else{
-            setPassMissMath("Password Missmatch")
-        }
-
+        if(!mail.endsWith("@gmail.com") || mail.length<"@gmail.com".length){
+        setErr("provide valid email")
+       }
+       else if(pass.length===0  ){
+        setPassErr("set Password")
+       }
+       else if(confPass!==pass){
+        setPassMissMath("mismatch")
+       }
+       else{
+          axios.post("http://localhost:5000/usersignup", { email: mail, password: pass })
+                .then((res) => {
+                    setSignupStatus(res.data.message)
+                    setTimeout(() => {
+                        navigate("/signin")
+                    }, 2000)
+                    setIsSignup(true)
+                    
+                })
+                .catch((err) => {
+                    if(err.response.status===409){
+                        setErr("email already exist")
+                    }
+                    else if(err.response.status===500){
+                        setSignupStatus(err.response.data.message)
+                    setIsSignup(false)
+                    }
+                    
+                })
+       }
+       
+       
+       
     }
-    axios.post("http://localhost:5000/usersignup",{email:mail,password:pass})
-    .then((res)=>{
-        setSignupStatus(res.data.message)
-        navigate("/signin")
-    })
-    .catch((err)=>{
-        setSignupStatus(err.data.message)
-    })
+
     return (
         <>
             <div className="grid grid-cols-12 w-full min-h-screen   bg-[radial-gradient(circle_at_20%_30%,rgba(255,255,255,0.25),transparent_40%),radial-gradient(circle_at_0%_0%,rgba(255,255,255,0.15),transparent_35%),linear-gradient(to_right,#000000,#000000)] ">
@@ -49,7 +56,7 @@ export default function Signup() {
                     <img src={google} alt="" className="object-contain cursor-pointer ml-3 hover:translate-y-2 hover:skew-1 " />
                     <p className=" mr-1 text-white">or</p>
                     <div className=" md:w-lg  ">
-                        <div className="h-24  w-full max-w-lg   flex flex-col justify-start gap-2 px-4 ">
+                        <div className="h-27  w-full max-w-lg   flex flex-col justify-start gap-2 px-4 ">
                             <label className="text-white text-left ">Email</label>
                             <input type="text" className=" pl-3 py-2 w-full max-w-lg   h-10 bg-black  text-white border border-white border-solid" placeholder="xyz@gmail.com" value={mail} onChange={(e) => { setMail(e.target.value) }} />
                             {err && <p className="text-red-500 text-sm  ">{err}</p>}
@@ -60,7 +67,7 @@ export default function Signup() {
                             {passErr && <p className="text-red-500 text-sm ">{passErr}</p>}
                             <label className="text-white text-left  ">Confirm Password</label>
                             <input type="password" className=" pl-3  w-full max-w-lg  h-10 bg-black  text-white border border-white border-solid" placeholder="(eg:world is not enough) " value={confPass} onChange={(e) => { setConfPass(e.target.value) }} />
-                             {passMissMatch && <p className="text-red-500 text-sm ">{passMissMatch}</p>}
+                            {passMissMatch && <p className="text-red-500 text-sm ">{passMissMatch}</p>}
 
                         </div>
                         <p className="text-left  text-white  px-4">
@@ -68,7 +75,7 @@ export default function Signup() {
                         </p>
 
                         <button className="w-36  text-lg mt-5 h-12 bg-white  font-thin hover:bg-black hover:text-white hover:translate-0.5 ml-4" onClick={handleCreateAccount}>Create Account</button>
-                        {signupStatus && <p className="text-sm text-green-500 mt-2 ml-4">{signupStatus}</p>}
+                        {isSignup !== null && <p className={`${isSignup?("text-green-500"):("text-red-500")} text-sm  mt-2 ml-4`}>{signupStatus}</p>}
                         <p className="text-left ml-4 text-white mt-1">already a user <a href="/signin" className="text-blue-900 hover:text-blue-400">Sign in</a></p>
                     </div>
 
